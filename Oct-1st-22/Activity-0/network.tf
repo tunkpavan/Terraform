85663c47adcd2f8d27e58e4a037e83229c1640c4
tunkpavan <59603032+tunkpavan@users.noreply.github.com>
2022-10-01 17:33:14 +0530

    resources to create in aws cloud
    VPC,Subnet,S3 Bucket

---
+++ Oct-1st-22/Activity-0/network.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "ntier" {
+    cidr_block      = var.network_cidr
+    tags            = {
+        Name        = "ntier"
+    } 
+}
+
+
+resource "aws_subnet" "subnets" {
+    count           = length(var.subnet_name_tags)
+    cidr_block      = cidrsubnet(var.network_cidr,8,count.index)
+    tags            = {
+        Name        = var.subnet_name_tags[count.index]
+    } 
+    availability_zone = format("${var.region}%s", count.index%2==0?"a":"b")
+    vpc_id          = aws_vpc.ntier.id 
+}
+
+
+
+
+
+
+resource "aws_internet_gateway" "ntier_igw" {
+    vpc_id          = aws_vpc.ntier.id
+    tags            = {
+        Name        = "ntier-igw"
+    } 
+
+}
+
+
+resource "aws_s3_bucket" "my_bucket" {
+    bucket          = var.bucket_name 
+
+}
+
