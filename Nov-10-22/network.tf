97aee56d6b0a50900af26ae21a0375e9da33aa05
tunkpavan <59603032+tunkpavan@users.noreply.github.com>
2022-11-10 09:51:00 +0530

    added route table association

--- Nov-10-22/network.tf
+++ Nov-10-22/network.tf
@@ -85,3 +85,9 @@ resource "aws_security_group" "appsg" {
     } 
 
 }
+
+resource "aws_route_table_association" "associations" {
+    count               = length(aws_subnet.subnets)
+    subnet_id           = aws_subnet.subnets[count.index].id
+    route_table_id      = contains(var.public_subnets, lookup(aws_subnet.subnets[count.index].tags_all, "Name", ""))?aws_route_table.publicrt.id :  aws_route_table.privatert.id
+}
