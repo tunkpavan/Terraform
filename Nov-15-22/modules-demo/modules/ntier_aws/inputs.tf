25b048fff96e2d5ad4339312b97a61abba55f4bb
tunkpavan <59603032+tunkpavan@users.noreply.github.com>
2022-11-15 12:22:42 +0530

    added input files

---
+++ Nov-15-22/modules-demo/modules/ntier_aws/inputs.tf
@@ -0,0 +1,25 @@
+variable "region" {
+    type    = string
+    default = "ap-southeast-1"
+}
+
+
+
+variable "subnet_name_tags" {
+    type    = list(string)
+    default = [ "web1", "web2", "app1", "app2", "db1", "db2"  ]
+
+}
+
+variable "network_cidr" {
+    type    = string
+    default = "10.0.0.0/16"
+
+}
+
+
+variable "bucket_name" {
+    type    = string
+    default = "qtsdetf3"
+
+}
